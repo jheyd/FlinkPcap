35cbff250a94f40445befd7c69295b48180619bd
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2016-07-08 09:31:40 +0200

    replaced jnetpcap-based solution with pcap4j-based solution

--- src/main/scala/pcap/FlinkPcap.scala
+++ src/main/scala/pcap/FlinkPcap.scala
@@ -1,35 +1,54 @@
 package pcap
 
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-import org.jnetpcap.Pcap
-import org.jnetpcap.packet.PcapPacket
-import org.jnetpcap.util.PcapPacketArrayList
+import java.net.Inet4Address
 
-import scala.collection.JavaConverters._
+import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.pcap4j.core.{PacketListener, Pcaps, RawPacketListener}
+import org.pcap4j.packet.factory.PacketFactory
+import org.pcap4j.packet.{IpV4Packet, Packet}
+
+import scala.collection.mutable
 
 object FlinkPcap {
+
+  implicit val typeInfo1 = TypeInformation.of(classOf[Array[Byte]])
+  implicit val typeInfo2 = TypeInformation.of(classOf[String])
+  implicit val typeInfo3 = TypeInformation.of(classOf[(String, Int)])
 
   def main(args: Array[String]) {
     val filename = "src/test/resources/200610041400.dump"
     val packetList = readPacketsFromFile(filename)
-    val packets = StreamExecutionEnvironment.getExecutionEnvironment.fromCollection(packetList)
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    val packets = env.fromCollection(packetList)
+    val keyedPackets = packets.keyBy(srcIp(_))
+    val windowedPackets = keyedPackets.countWindow(100)
+    val totalSizesByKey = windowedPackets.fold(("", 0))((accumulator: (String, Int), value: Array[Byte]) => {
+      val addr = srcIp(value)
+      val newLength = accumulator._2 + value.length
+      (addr, newLength)
+    })
+    totalSizesByKey.print()
+
+    env.execute()
   }
 
-  def readPacketsFromFile(filename: String): Seq[PcapPacket] = {
-    val errorBuffer = new java.lang.StringBuilder()
-    val pcap = Pcap.openOffline(filename, errorBuffer)
-
-    if (pcap == null) {
-      print(errorBuffer)
-      System.exit(-1)
+  def readPacketsFromFile(filename: String): Seq[Array[Byte]] = {
+    val handle = Pcaps.openOffline(filename)
+    val packetBuffer = mutable.Buffer[Array[Byte]]()
+    val listener = new RawPacketListener {
+      override def gotPacket(packetBytes: Array[Byte]): Unit = {
+        packetBuffer += packetBytes
+      }
     }
+    handle.dispatch(10000, listener)
 
-    try {
-      val packets = new PcapPacketArrayList
-      pcap.loop(-1, packets, null)
-      return packets.asScala
-    } finally {
-      pcap.close
-    }
+    packetBuffer.toSeq
   }
+
+  def srcIp(rawPacket: Array[Byte]): String = {
+    val ipPacket = new IpV4Packet(rawPacket)
+    ipPacket.getHeader.getSrcAddr.getHostAddress
+  }
+
 }
