9d7dd65c662ac17a5f7a91f679b8e5747a6f7ed7
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2016-07-12 20:55:06 +0200

    moved ip packet extraction functionality from IpPacketBytesPerKeyAnalyser to new super trait IpIntAnalyser

---
+++ src/main/scala/pcap/analysers/ints/ippacketbytes/IpIntAnalyser.scala
@@ -0,0 +1,23 @@
+package pcap.analysers.ints.ippacketbytes
+
+import org.pcap4j.packet.EthernetPacket
+import pcap.analysers.ints.IntAnalyser
+
+trait IpIntAnalyser extends IntAnalyser {
+
+  def ipBasedKey(rawIpPacket: Array[Byte]): String
+
+  def ipBasedValue(rawIpPacket: Array[Byte]): Int
+
+  override def key(rawEthernetPacket: Array[Byte]): String = ipBasedKey(extractRawIpPacket(rawEthernetPacket))
+
+  override def value(rawEthernetPacket: Array[Byte]): Int = ipBasedValue(extractRawIpPacket(rawEthernetPacket))
+
+  def extractRawIpPacket(rawEthernetPacket: Array[Byte]): Array[Byte] = {
+    val ethernetPacket = EthernetPacket.newPacket(rawEthernetPacket, 0, rawEthernetPacket.length)
+    val rawIpPacket = ethernetPacket.getPayload.getRawData
+    rawIpPacket
+  }
+
+
+}
--- src/main/scala/pcap/analysers/ints/ippacketbytes/IpPacketBytesPerKeyAnalyser.scala
+++ src/main/scala/pcap/analysers/ints/ippacketbytes/IpPacketBytesPerKeyAnalyser.scala
@@ -3,21 +3,10 @@ package pcap.analysers.ints.ippacketbytes
 import org.pcap4j.packet.EthernetPacket
 import pcap.analysers.ints.IntAnalyser
 
-trait IpPacketBytesPerKeyAnalyser extends IntAnalyser{
+trait IpPacketBytesPerKeyAnalyser extends IpIntAnalyser {
 
-  def ipBasedKey(rawIpPacket: Array[Byte]): String
-
-  override def key(rawEthernetPacket: Array[Byte]): String = ipBasedKey(extractRawIpPacket(rawEthernetPacket))
+  override def ipBasedValue(rawIpPacket: Array[Byte]): Int = rawIpPacket.length
 
   override def aggregate(left: Int, right: Int): Int = left + right
-
-  override def value(rawEthernetPacket: Array[Byte]): Int = extractRawIpPacket(rawEthernetPacket).length
-
-  def extractRawIpPacket(rawEthernetPacket: Array[Byte]): Array[Byte] = {
-    val ethernetPacket = EthernetPacket.newPacket(rawEthernetPacket, 0, rawEthernetPacket.length)
-    val rawIpPacket = ethernetPacket.getPayload.getRawData
-    rawIpPacket
-  }
-
 
 }
