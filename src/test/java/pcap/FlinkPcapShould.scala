2bca58fa53a2abd7f77672f8fcb493af1aca7a54
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2016-07-27 21:42:18 +0200

    clean up test

--- src/test/java/pcap/FlinkPcapShould.scala
+++ src/test/java/pcap/FlinkPcapShould.scala
@@ -12,23 +12,22 @@ import scala.Tuple2
 import org.junit.Assert.assertEquals
 
 class FlinkPcapShould {
-  private val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
   implicit val typeInfo1 = TypeInformation.of(classOf[Int])
 
   @org.junit.Test
-  def analyzeBytesPerSrcIpFrom1000PackagesWith258ResultElements {
+  def analyzeBytesPerSrcIpFrom1000PackagesWith258ResultElements() {
     val resultLength: Long = runAnalysis(new IpPacketBytesPerSrcIpAnalyser).count
     assertEquals(258, resultLength)
   }
 
   @org.junit.Test
-  def analyzeBytesPerDestIpFrom1000PackagesWith250ResultElements {
+  def analyzeBytesPerDestIpFrom1000PackagesWith250ResultElements() {
     val resultLength: Long = runAnalysis(new IpPacketBytesPerDestIpAnalyser).count
     assertEquals(250, resultLength)
   }
 
   @org.junit.Test
-  def analyzeBytesPerPortsFrom1000PackagesWith363ResultElements {
+  def analyzeBytesPerPortsFrom1000PackagesWith363ResultElements() {
     val resultLength: Long = runAnalysis(new IpPacketBytesPerPortsAnalyser).count
     assertEquals(363, resultLength)
   }
